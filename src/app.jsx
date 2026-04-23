a8f2fc6b2ffbbfef101027804cee32ccf8327b4b
ArchiZada Santa <224439736+ahadaliofficial@users.noreply.github.com>
2026-04-23 15:18:06 +0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BharatAssets from "./Bharatassets"; 
-import BharatAdmin from "./Bharatadmin";
+import BharatAdmin from "./Bharatadmin";   
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* This shows at: your-link.vercel.app/ */}
         <Route path="/" element={<BharatAssets />} />
+        
+        {/* This shows at: your-link.vercel.app/admin */}
         <Route path="/admin" element={<BharatAdmin />} />
       </Routes>
     </BrowserRouter>
