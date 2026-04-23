05fdbabefb84da8d27745abb05b75d9c504df9fb
ArchiZada Santa <224439736+ahadaliofficial@users.noreply.github.com>
2026-04-23 15:05:18 +0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BharatAssets from "./BharatAssets"; 
-import BharatAdmin from "./BharatAdmin";
+import BharatAssets from "./Bharatassets"; 
+import BharatAdmin from "./Bharatadmin";
 
 export default function App() {
   return (
